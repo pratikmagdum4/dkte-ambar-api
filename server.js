7d9b9432026d262ef6faac51d4cf837c6274bc11
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-07-12 21:48:48 +0530

    fixdeploy try 6

--- server.js
+++ server.js
@@ -13,19 +13,22 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.json({ message: "hello from Amber server" });
+});
 
 // Routes
+
 console.log("hi there ");
 app.use("/api/facultyAchievements", facultyAchievementRoutes);
 // app.use("/api/studentAchievements", studentAchievementRoutes);
-app.get("/",(req,res)=>{
-    res.json({message:"hello from Amber server"});
-})
+
 app.listen(PORT, () => {
   connectToDB();
   console.log(chalk.blueBright(`Server Running on port ${PORT}`));
