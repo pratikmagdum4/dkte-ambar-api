8eebcfed8f9cc06cc1f0672352f18a00b81e4d81
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-07-12 21:33:28 +0530

    fix:deploy 4

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import chalk from "chalk";
 import cookieParser from "cookie-parser";
-import connectToDB from "../src/db/connectToDB.js";
-import cors from "cors"
-import facultyAchievementRoutes from "../src/routes/facultyAchievementRoutes.js"
+import connectToDB from "./src/db/connectToDB.js";
+import cors from "cors";
+import facultyAchievementRoutes from "./src/routes/facultyAchievementRoutes.js";
 // import studentAchievementRoutes from "./src/routes/studentAchievementRoutes.js"
 import bodyParser from "body-parser";
 
@@ -20,11 +20,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-console.log("hi there ")
+console.log("hi there ");
 app.use("/api/facultyAchievements", facultyAchievementRoutes);
 // app.use("/api/studentAchievements", studentAchievementRoutes);
-
+app.get("/",(req,res)=>{
+    res.json({message:"hello from Amber server"});
+})
 app.listen(PORT, () => {
-    connectToDB();
-    console.log(chalk.blueBright(`Server Running on port ${PORT}`)); 
+  connectToDB();
+  console.log(chalk.blueBright(`Server Running on port ${PORT}`));
 });
